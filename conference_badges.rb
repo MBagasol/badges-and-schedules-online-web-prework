9110e60da69c56102ac8c03f335e6df685876f92
Mark Anthony Bagasol <markbagasol@gmail.com>
2019-06-08 04:00:20 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,8 +3,8 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator
+def batch_badge_creator(attendees)
   attendees.map do |attendee|
-    "Hello, my name is #{attendee}"
+    "Hello, my name is #{attendee}."
   end
 end
